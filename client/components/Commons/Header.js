01343cba1a03b6a9ba78b1ad75cbae9a592832fb
SimranKaur <simrankaur.adept@gmail.com>
2016-09-26 22:14:56 +0530

    gommet header

--- client/components/Commons/Header.js
+++ client/components/Commons/Header.js
@@ -3,29 +3,40 @@
  */
 
 import React, {PropTypes}  from 'react';
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
-//import {Header, Title, Menu, Anchor, Navbar} from 'grommet';
+//import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import {Header, Title, Menu, Anchor, Navbar} from 'grommet';
 
 
 
 const TopHeader = (props) => {
   return(
-    <div>
-      <Navbar color="faded" light>
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
-        <Nav className="pull-xs-right" navbar>
-          <NavItem>
-            <NavLink href="/components/">Components</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
-          </NavItem>
-        </Nav>
-      </Navbar>
-    </div>
-
+    <Menu inline={true} direction="row" size="large">
+      <Anchor href="#" className="active">
+        First action
+      </Anchor>
+      <Anchor href="#">
+       Second action
+     </Anchor>
+      <Anchor href="#">
+        Third action
+     </Anchor>
+    </Menu>
   );
 }
+//
+// <div>
+//   <Navbar color="faded" light>
+//     <NavbarBrand href="/">reactstrap</NavbarBrand>
+//     <Nav className="pull-xs-right" navbar>
+//       <NavItem>
+//         <NavLink href="/components/">Components</NavLink>
+//       </NavItem>
+//       <NavItem>
+//         <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
+//       </NavItem>
+//     </Nav>
+//   </Navbar>
+// </div>
 
 export default TopHeader;
 
